9529bda8890995608c3016e260e28a0cc2f992cb
Vishakha Kalyankar <vishakha.k@highradius.com>
2021-03-31 09:40:02 +0530

    commiting second file

---
+++ MyFile.java
@@ -0,0 +1,10 @@
+package com.highradius.administration;
+
+public class MyFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("In My file");
+	}
+
+}
